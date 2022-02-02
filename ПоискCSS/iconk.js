944c14441abe8e9caff3dcca27a6af50fbe12c04
KefirNalivai <98163662+KefirNalivai@users.noreply.github.com>
2022-02-02 15:05:41 +0300

    Add files via upload

---
+++ ПоискCSS/iconk.js
@@ -0,0 +1,24 @@
+﻿function putsearch() {
+var obj = window.document.getElementById("Поиск");
+obj.value = obj.value.replace( /^\s+/g, '');
+obj.value = obj.value.replace( /\s+$/g, '');
+
+if(obj.value!="")
+{
+localStorage.setItem("searchkey",obj.value);
+obj.value = "";
+ document.location.href = "Поиск.html";
+}
+else
+{
+alert ("Вы ничего не ввели для поиска");
+}
+}
+
+function prsearch() {
+var word = localStorage.getItem("searchkey");
+
+var obj = window.document.getElementById("text-to-find");
+obj.value = word;
+localStorage.removeItem("searchkey");
+}
---
+++ ПоискCSS/poisk.js
@@ -0,0 +1,52 @@
+﻿ function search2() {
+var textToFind = localStorage.getItem("search");
+if(textToFind != null)
+{
+document.body.innerHTML = document.body.innerHTML.replace(eval("/"+textToFind+"/gi"),"<span name="+textToFind+" style=' background:blue;'>"+textToFind+"</span>");
+localStorage.removeItem("search");
+}
+}
+
+function search() {
+var ix = 1;
+while(ix<40)
+{
+
+ var header = document.querySelector('.texxt p:nth-child('+ix+')');
+if(header!=null)
+{
+  var word = localStorage.getItem("search");
+    regexp = new RegExp(word, 'i');
+ 
+    var reg = new RegExp(word, 'g');
+    header.innerHTML = header.innerHTML.replace(reg, '<span style="font-weight: bold; background:pink;">' + word + '</span>');
+ } 
+ix = ix+1;
+}
+ix=1;
+while(ix<40)
+{
+
+ var header = document.querySelector('.texxt p:nth-child('+ix+')');
+if(header!=null)
+{
+  var word = localStorage.getItem("search2");
+    regexp = new RegExp(word, 'i');
+ 
+    var reg = new RegExp(word, 'g');
+    header.innerHTML = header.innerHTML.replace(reg, '<span style="font-weight: bold; background:pink;">' + word + '</span>');
+ } 
+ix = ix+1;
+}
+
+
+
+
+
+
+ 
+
+localStorage.removeItem("search");
+localStorage.removeItem("search2");
+
+}
